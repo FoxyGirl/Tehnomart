a13759c3c6ccf273bf8b6679d7207786b8caa72b
Elena Pavlova <lenlho22@gmail.com>
2016-07-21 16:23:42 +0300

    Create script for Modal form

--- js/custom_script.js
+++ js/custom_script.js
@@ -1,5 +1,62 @@
 (function () {
   'use strict';
   
+  var modalShadow = document.getElementById('modalShadow'),
+      modalContactUs = document.getElementById('modalContactUs'),
+      formContactUs = modalContactUs.querySelector('form[name=formContactUs]'),
+      formFields = formContactUs.querySelectorAll('input, textarea'),
+      writeUs = document.getElementById('writeUs');      
+  
+  function hideModal() {
+    modalShadow.classList.remove('show');
+    modalContactUs.classList.remove('show');    
+  }
+  
+  function clearForm() {
+    for (var i=0; i < formFields.length; i++) {
+      formFields[i].value = '';
+    }
+  }
+  
+  /*****************/
+  
+  for (var i=0; i < formFields.length; i++) {
+    formFields[i].removeAttribute('required');
+  }
+  
+  writeUs.addEventListener('click', function (e) {
+    e.preventDefault();
+    clearForm();
+    modalShadow.classList.add('show');
+    modalContactUs.classList.add('show');
+    formFields[0].focus();
+  });
+  
+  formContactUs.addEventListener('submit', function (e) {
+    e.preventDefault();
+    var errors = [];
+    
+    for (var i=0; i < formFields.length; i++) {
+      if ( formFields[i].value == '' ) {        
+        formFields[i].style.borderColor = 'red'; 
+        errors.push(formFields[i]);        
+      } else {
+        formFields[i].style.borderColor = '';
+      };
+    };
+          
+    if ( errors.length > 0 ) {
+        errors[0].focus();
+        return false;
+    } else { 
+      hideModal();
+      return true; 
+    };
+    
+  });
+  
+  formContactUs.addEventListener('reset', function () {
+      hideModal();
+  });
   
 })();
